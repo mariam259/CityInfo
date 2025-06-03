7ffd3e488730701d2b2c7c5ba852f9bf262549b9
mariam259 <mariamahmed6441@gamil.com>
2025-06-03 03:04:56 +0300

    return status code

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -2,6 +2,7 @@
 // using System.Collections.Generic;
 // using System.Linq;
 // using System.Threading.Tasks;
+using CityInfo.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
@@ -11,23 +12,41 @@ namespace CityInfo.API.Controllers
     public class CitiesController : ControllerBase
     {
         [HttpGet()]
-        public JsonResult GetCities()
-        {
-            // return new JsonResult(
-            //      new List<object>
-            //      {
-            //         new {id = 1 , Name = "Cairo"},
+        // Older version of code
+        // public JsonResult GetCities()
+        // {
+        //     // return new JsonResult(
+        //     //      new List<object>
+        //     //      {
+        //     //         new {id = 1 , Name = "Cairo"},
 
-            //         new {id = 2 , Name = "Alex"}
-            //      }
-            //  );
-            //updated code is to use city model
-            return new JsonResult(CitiesDataStores.Current.Cities);
+        //     //         new {id = 2 , Name = "Alex"}
+        //     //      }
+        //     //  );
+        //     //updated code is to use city model
+        //     return new JsonResult(CitiesDataStores.Current.Cities);
+        // }
+        public ActionResult<IEnumerable<CityDto>> GetCities()
+        {
+            return Ok(CitiesDataStores.Current.Cities);
         }
         [HttpGet("{id}")]
-        public JsonResult GetCity(int id)
+        // Older version of code
+        // public JsonResult GetCity(int id)
+        // {
+        //     return new JsonResult(CitiesDataStores.Current.Cities.FirstOrDefault(city => city.Id == id));
+        // }
+        public ActionResult<CityDto> GetCity(int id)
         {
-            return new JsonResult(CitiesDataStores.Current.Cities.FirstOrDefault(city => city.Id == id));
+            //find the city
+            var cityToReturn = CitiesDataStores.Current.Cities.FirstOrDefault(city => city.Id == id);
+            if (cityToReturn == null)
+            {
+                return NotFound();
+            }
+            return Ok(cityToReturn);
+
+
         }
     }
 }
