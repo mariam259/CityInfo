e0d632ff2c9343b57ac668106124ab79a8e0fd93
mariam259 <mariamahmed6441@gamil.com>
2025-06-10 23:44:41 +0300

    add validation annotation

---
+++ Models/PointOfInterestForUpdateDto.cs
@@ -0,0 +1,20 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.API.Models
+{
+    public class PointOfInterestForUpdateDto
+    {
+        [Required(ErrorMessage = "you must provide a name")]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(200)]
+        public string? Description { get; set; }
+
+    }
+}
