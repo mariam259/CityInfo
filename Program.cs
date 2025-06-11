643ab476966e3a348949c7e35fbad079d15c9aa6
mariam259 <mariamahmed6441@gamil.com>
2025-06-12 00:02:08 +0300

    add logger and new services

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using CityInfo.API.Services;
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+// builder.Logging.ClearProviders();
+// builder.Logging.AddConsole();
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -11,6 +23,7 @@ builder.Services.AddControllers(options =>
 ).AddNewtonsoftJson().
 AddXmlDataContractSerializerFormatters();
 
+builder.Services.AddProblemDetails();
 //add new attribute for http error response body called additionalInfo
 /*builder.Services.AddProblemDetails(options =>
  options.CustomizeProblemDetails = ctx =>
@@ -28,10 +41,17 @@ AddXmlDataContractSerializerFormatters();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
+builder.Services.AddTransient<LocalMailService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
